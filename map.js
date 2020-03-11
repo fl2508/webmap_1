4f260826c97838dad19780ff5c9f6ac46ed8472e
fl2508 <fl2508@barnard.edu>
2020-03-10 23:45:50 -0400

    zoom

--- map.js
+++ map.js
@@ -7,7 +7,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZmwyNTA4IiwiYSI6ImNrN21uZjJ0dTA3dzYzaG4ybDdpa
 let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v10',
-    center: [-74.0060, 40.7128],
+    center: [-73.9465, 40.8116],
     zoom: 15
 })
 
