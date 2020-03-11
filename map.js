f8832e271652b8a2db2a6312df1296eb655c9894
fl2508 <fl2508@barnard.edu>
2020-03-10 22:23:59 -0400

    center nyc

--- map.js
+++ map.js
@@ -24,3 +24,30 @@ let scale = new mapboxgl.ScaleControl({
 })
 
 map.addControl(scale, 'bottom-right')
+
+let data = [
+    {
+        location: [-73.96191,40.80762],
+        content: 'I like to eat my lunch here'
+    },
+    {
+        location: [-73.95936,40.80610],
+        content: '15 years ago, you could see over the trees'
+    },
+    {
+        location: [-73.96204,40.80994],
+        content: 'This was once tennis courts'
+    },
+    ]
+
+data.forEach(function(d) {
+
+    let marker = new mapboxgl.Marker()
+    marker.setLngLat(d.location)
+    marker.addTo(map)
+
+    let popup = new mapboxgl.Popup()
+    popup.setHTML(d.content)
+    marker.setPopup(popup)
+
+})
