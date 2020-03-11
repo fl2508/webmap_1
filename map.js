884ca23938c136638721960f7338d6f721621349
fl2508 <fl2508@barnard.edu>
2020-03-10 21:37:53 -0400

    added javascript component for map

--- map.js
+++ map.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!")
+'use strict'
+
+console.log('Loaded map.js')
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmwyNTA4IiwiYSI6ImNrN21uZjJ0dTA3dzYzaG4ybDdpa2p5MzQifQ.zI-jnQ5M6ft68VVJR3XYMQ'
+
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [-73.96024, 40.80877],
+    zoom: 1
+})
